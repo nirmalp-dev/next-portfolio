f4619360aee1efe6033c01c3a43ccc0270266d97
Nirmal Patel <nirmalypatel@gmail.com>
2024-10-29 23:13:11 -0500

    project slug change

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ###  Portfolio website 
+
+The Aceternity UI template has been used and modified for the large part of the website. Huge Thanks to the team.   
--- src/constants/products.tsx
+++ src/constants/products.tsx
@@ -1,15 +1,11 @@
 import sidefolioAceternity from "public/images/sidefolio-aceternity-2.png";
 import sidefolioAceternity2 from "public/images/sidefolio-aceternity-2.png";
-import sidefolioAlgochurn from "public/images/sidefolio-algochurn.png";
-import sidefolioAlgochurn2 from "public/images/sidefolio-algochurn.png";
-import sidefolioMoonbeam from "public/images/sidefolio-moonbeam.png";
-import sidefolioMoonbeam2 from "public/images/sidefolio-moonbeam-2.png";
 import sidefolioTailwindMasterKit from "public/images/sidefolio-tailwindmasterkit.png";
 import sidefolioTailwindMasterKit2 from "public/images/sidefolio-tailwindmasterkit-2.png";
 
 export const products = [
   {
-    href: "",
+    href: "e-commerce-platform",
     title: "E-Commerce Platform",
     description:
         "A full-stack, production-ready e-commerce platform with modern technologies, containerized with Docker for " +
@@ -46,7 +42,7 @@ export const products = [
     ),
   },
   {
-    href: "",
+    href: "medic-ai",
     title: "Medic.AI",
     description:
       "A platform to help patient get the right data in one place",
